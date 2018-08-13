0ce1a05fd4265e17cd2a38638a50df9645f5315f
Unknown <seguragabriel@unbosque.edu.co>
2018-08-13 10:42:23 -0500

    plugins folder
    theme folder inside

---
+++ plugins/themes/default 2/js/see-more-txt.js
@@ -0,0 +1,101 @@
+/**
+ * @file plugins/themes/default/js/main.js
+ *
+ * Copyright (c) 2014-2017 Simon Fraser University
+ * Copyright (c) 2000-2017 John Willinsky
+ * Distributed under the GNU GPL v2. For full terms see the file docs/COPYING.
+ *
+ * @brief Handle JavaScript functionality unique to this theme.
+ */
+
+/*
+$(document).ready(function() {
+    // Configure/customize these variables.
+    var showChar = 300;  // How many characters are shown by default
+    var ellipsestext = "...";
+    var moretext = "Ver Más";
+    var lesstext = "Ver Menos";
+    
+
+    $('.more').each(function() {
+        var content = $(this).html();
+ 
+        if(content.length > showChar) {
+ 
+            var c = content.substr(0, showChar);
+            var h = content.substr(showChar, content.length - showChar);
+ 
+            //var html = c + '<span class="moreellipses">' + ellipsestext+ '&nbsp;</span><span class="morecontent"><span>' + h + '</span>&nbsp;&nbsp;<a href="" class="morelink">' + moretext + '</a></span>';
+ 
+            $(this).html(html);
+        }
+ 
+    });
+ 
+    $(".morelink").click(function(){
+        if($(this).hasClass("less")) {
+            $(this).removeClass("less");
+            $(this).html(moretext);
+        } else {
+            $(this).addClass("less");
+            $(this).html(lesstext);
+        }
+        $(this).parent().prev().toggle();
+        $(this).prev().toggle();
+        return false;
+    });
+});
+*/
+
+$(function(){
+	$(".read-more").readmore({
+		hideText: "Read Less...",
+		readText: "Read More...",
+		isTextShown: false,
+		effect: true,
+		effectOption: "fast",
+		buttonClasses: "btn-primary opacity-rollover",
+		dataid: "read-more-action",
+		multiple: false
+	});
+});
+
+$(function() {
+    // Configure/customize these variables.
+    var showChar = 100;  // How many characters are shown by default
+    var ellipsestext = "...";
+    var moretext = "Show more >";
+    var lesstext = "Show less";
+    
+
+    $('.more').each(function() {
+        var content = $(this).html();
+ 
+        if(content.length > showChar) {
+ 
+            var c = content.substr(0, showChar);
+            var h = content.substr(showChar, content.length - showChar);
+ 
+            var html = c + '<span class="moreellipses">' + ellipsestext+ '&nbsp;</span><span class="morecontent"><span>' + h + '</span>&nbsp;&nbsp;<a href="" class="morelink">' + moretext + '</a></span>';
+ 
+            $(this).html(html);
+        }
+ 
+    });
+ 
+    $(".morelink").click(function(){
+        if($(this).hasClass("less")) {
+            $(this).removeClass("less");
+            $(this).html(moretext);
+        } else {
+            $(this).addClass("less");
+            $(this).html(lesstext);
+        }
+        $(this).parent().prev().toggle();
+        $(this).prev().toggle();
+        return false;
+    });
+});
+
+
+
---
+++ plugins/themes/default/js/google-translate.js
@@ -0,0 +1,6 @@
+function googleTranslateElementInit() {
+  new google.translate.TranslateElement({
+    // here is where you change the language
+    //pageLanguage: 'en'
+  }, 'google_translate_element');
+}
